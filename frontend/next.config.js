9f68e72844adaf1e9f28699fa31a8e8584eccaac
vmulu <144148151+vmulu@users.noreply.github.com>
2024-11-12 12:32:56 -0500

    connected backend with front end and added login page for students file

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,7 +2,9 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  // Your Next.js configuration
+  env: {
+    NEXT_PUBLIC_API_BASE_URL: "http://127.0.0.1:8000", 
+  },
 };
 
 module.exports = nextConfig;
